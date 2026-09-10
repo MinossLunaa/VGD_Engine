0aadc5ba63749a75755552e30852f286444f1740
MinossLunaa <lpbowmanlinehan@email.neit.edu>
2026-09-10 10:51:41 -0400

    updated!!!!!!!!!!!

--- vgd126_engine/js/gamestates/menu.js
+++ vgd126_engine/js/gamestates/menu.js
@@ -4,8 +4,12 @@
  */
 
 var startButton = new GameObject();
-startButton.width = 200;
+startButton.img.src = "images/start_red.png";
+startButton.height = 150;
+startButton.width = 300;
 startButton.hitBoxWidth = 800;
+startButton.x = 500;
+startButton.y = 400;
 
 var menuBackground = new GameObject();
 
@@ -13,7 +17,7 @@ var menuBackground = new GameObject();
    ✏️ STUDENT EDIT ZONE: MENU BACKGROUND IMAGE
    Replace with your own JPG/PNG; keep aspect ratio close to canvas.
    ===================================================================== */
-menuBackground.img.src = "images/mrt.jpg";
+menuBackground.img.src = "images/MainMenuTerry.png";
 menuBackground.width = canvas.width;
 menuBackground.height = canvas.height;
 
@@ -22,13 +26,12 @@ gameStates[`menu`] = function () {
   if (startButton.overlap(mouse)) {
       if (mouse.pressed) {
           gameStates.changeState(`level1`),  sounds.play(`backgroundSFX`, 0, loop = true);
-
       }
-    startButton.color = `yellow`;
+      startButton.img.src = "images/start_black.png";
   } else {
-    startButton.color = `red`;
+      startButton.img.src = "images/start_red.png";
   }
 
   menuBackground.drawStaticImage();
-  startButton.render();
+  startButton.drawStaticImage();
 };
